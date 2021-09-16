ddc0d257181c2840d987885443031975a48d721b
Matthew Little <zone117x@gmail.com>
2021-09-16 12:16:14 +0200

    feat: log unique nodes within private IP ranges

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,27 @@ function wait(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/*
+  "network_id": 1,
+  "peer_version": 402653184,
+  "ip": "50.17.99.78",
+  "port": 20444,
+  "public_key_hash": "2073870d87637302a3cabe39bc9682ddda836302",
+  "authenticated": true
+*/
+
+type IPv4 = `${string}.${string}.${string}.${string}`;
+type NeighborID = `${IPv4}@${string}`;
+
+type NeighborMap = Map<NeighborID, Neighbor>;
+
 interface Neighbor {
-  ip: string;
+  network_id: number;
+  peer_version: number;
+  ip: IPv4;
+  port: number;
+  public_key_hash: string;
+  authenticated: boolean;
 }
 
 interface Neighbors {
@@ -28,6 +47,7 @@ interface Neighbors {
 interface QueryResult {
   responsive: boolean;
   neighbors: Set<string>;
+  uniqueNeighbors: NeighborMap;
 }
 
 const RPC_PORT = 20443;
@@ -41,6 +61,7 @@ function getQueryUrls(nodeUrl: string): string[] {
 
 async function queryNodeNeighbors(nodeUrl: string, retries = 16): Promise<QueryResult> {
   const ips = new Set<string>();
+  const uniqueNeighbors = new Map<NeighborID, Neighbor>();
   let responsive = false;
   for (const queryUrl of getQueryUrls(nodeUrl)) {
     for (let i = 0; i < retries; i++) {
@@ -50,11 +71,13 @@ async function queryNodeNeighbors(nodeUrl: string, retries = 16): Promise<QueryR
         const ac = new AbortController();
         setTimeout(() => ac.abort(), 15000);
         const req = await fetch(queryNeighborsUrl, { signal: ac.signal as any });
-        const result: Neighbors = await req.json();
+        const result = await req.json() as Neighbors;
         responsive = true;
-        [...result.sample, ...result.inbound, ...result.outbound].forEach(n => ips.add(n.ip));
+        const allNeighbors = [...result.sample, ...result.inbound, ...result.outbound];
+        allNeighbors.forEach(n => ips.add(n.ip));
+        allNeighbors.forEach(n => uniqueNeighbors.set(`${n.ip}@${n.public_key_hash}`, n));
       } catch (error) {
-        console.info(`Neighbors RPC failed for ${nodeUrl}: ${error.message}`);
+        console.info(`Neighbors RPC failed for ${nodeUrl}: ${(error as Error).message}`);
         if (i < retries) {
           await wait(1500);
         }
@@ -62,15 +85,16 @@ async function queryNodeNeighbors(nodeUrl: string, retries = 16): Promise<QueryR
     }
   }
   console.log(`Node ${nodeUrl} has ${ips.size} neighbors`);
-  return { neighbors: ips, responsive };
+  return { neighbors: ips, responsive, uniqueNeighbors };
 }
 
 async function scanNeighbors() {
+  const uniqueNeighbors = new Map<string, Neighbor>();
   const foundIps = new Set<string>();
   const responsiveIps = new Set<string>();
   const queriedIps = new Set<string>();
 
-  const requestQueue = new pQueue({concurrency: 250});
+  const requestQueue = new pQueue({concurrency: 50});
 
   const getIpsToQuery = () => {
     const ips = new Set<string>();
@@ -93,6 +117,12 @@ async function scanNeighbors() {
           responsiveIps.add(ip);
         }
         result.neighbors.forEach(n => foundIps.add(n));
+        result.uniqueNeighbors.forEach((v, k) => {
+          if (!uniqueNeighbors.has(k)) {
+            console.log(`Found unique neighbor: ${k}`);
+            uniqueNeighbors.set(k, v);
+          }
+        });
         queueQueries();
       });
     });
@@ -107,28 +137,40 @@ async function scanNeighbors() {
     responsiveIps.delete(n);
   });
   foundIps.delete('0.0.0.0');
-  
-  const privateNetworks = [...foundIps].map(ip => new Address4(ip));
+
+  uniqueNeighbors.forEach((n, key) => {
+    if (BigInt('0x' + n.public_key_hash) === 0n || n.ip === '0.0.0.0') {
+      uniqueNeighbors.forEach((n1, k1) => {
+        if (n1.ip === n.ip && k1 !== key) {
+          uniqueNeighbors.delete(k1);
+        }
+      });
+    }
+  });
+
   const ipSubnetClassA = new Address4('10.0.0.0/8');
   const ipSubnetClassB = new Address4('172.16.0.0/12');
   const ipSubnetClassC = new Address4('192.168.0.0/16');
-  const privateIps = privateNetworks
+  const privateIps = [...foundIps]
+    .map(ip => new Address4(ip))
     .filter(ip => ip.isInSubnet(ipSubnetClassA) || ip.isInSubnet(ipSubnetClassB) || ip.isInSubnet(ipSubnetClassC))
     .map(ip => ip.address);
 
+  const publicIps = [...foundIps].filter(ip => !privateIps.includes(ip));
+
   console.log('-------');
-  console.log('IPs:');  
-  console.log([...foundIps].sort().join('\n'));
+  console.log('Neighbors:');
+  console.log([...uniqueNeighbors.keys()].sort().join('\n'));
   console.log('-------');
   console.log('Public nodes:');
   console.log([...responsiveIps].sort().join('\n'));
   console.log('-------');
-  console.log(`Found ${foundIps.size} nodes (${responsiveIps.size} with public RPC, ${privateIps.length} with private IP ranges`);
+  console.log(`Found ${uniqueNeighbors.size} nodes (${privateIps.length} with private IPs, ${publicIps.length} with public IPs, ${responsiveIps.size} with public RPC)`);
 
   const countries = new Map<string, number>();
-  foundIps.forEach(ip => {
-    const result = geoipCountry.lookup(ip)
-    const country = result?.country ?? '??';
+  uniqueNeighbors.forEach(n => {
+    const result = geoipCountry.lookup(n.ip)
+    const country = result?.country ?? 'Private';
     let count = countries.get(country) ?? 0;
     countries.set(country, ++count);
   });
